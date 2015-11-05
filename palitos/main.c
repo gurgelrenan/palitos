3213e794bffc680393cc1d8c5797bb1281ef39ae
Renan Gurgel <gurgel.renan@gmail.com>
2015-11-04 22:18:11 -0300

    Initial commit

---
+++ palitos/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  palitos
+//
+//  Created by Renan Gurgel on 11/4/15.
+//  Copyright © 2015 vitoria. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
