1270afb7b451c723d76e5af538f6b060f268407e
Renan Gurgel <gurgel.renan@gmail.com>
2015-11-04 23:20:14 -0300

    Add more requirements

--- palitos/main.c
+++ palitos/main.c
@@ -9,7 +9,52 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int matricula;
+    printf("Digite sua matrícula: \n");
+    scanf("%d",&matricula);
+    
+//   Comecar a transformar um inteiro em um array
+    /* count number of digits */
+    int c = 0; /* digit position */
+    int n = matricula;
+    
+    while (n != 0)
+    {
+        n /= 10;
+        c++;
+    }
+    
+    int numberArray[c];
+    
+    c = 0;
+    n = matricula;
+    
+    /* extract each digit */
+    while (n != 0)
+    {
+        numberArray[c] = n % 10;
+        n /= 10;
+        c++;
+    }
+    
+//    finaliza a transformacao de um inteiro em um array
+
+//	Comeca a mostrar a soma da matricula
+    int i;
+    int sum = 0;
+    
+    for (i=0;i<c;i++) {
+        printf("Posicao %d: ", i);
+        printf("%d", numberArray[i]);
+        printf("\n");
+        sum = sum + numberArray[i];
+    }
+    
+    int sum_matricula = sum;
+    
+    printf("A soma da sua matricula é: %d \n", sum_matricula);
+	
+//	fim soma matricula
+	
     return 0;
 }
